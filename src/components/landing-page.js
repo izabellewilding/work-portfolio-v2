cca9a08c2afbf4882a55f342c2235ec2a9112989
izabelle wilding <izabellewilding@gmail.com>
2020-02-26 11:10:06 +0000

    Landing page pain splatter SVGs

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -24,7 +24,29 @@ const LandingPage = () => {
   return (
     //landing page top
     <main className="gradient w-full h-screen relative items-center justify-center ">
-      {" "}
+      <div className="relative w-full h-screen">
+        <Splash
+          className=" absolute lightSVG bottom-0 splash z-10 "
+          style={{ height: "35vh" }}
+        />
+        <SplashMedium
+          className=" absolute bottom-0  lightSVG splash-med-left z-10 invisible sm:visible"
+          style={{ height: "50vh" }}
+        />
+        <Splash
+          className=" absolute lightSVG top-0 splash2 "
+          style={{ height: "35vh" }}
+        />
+        {/* <BlobSplash
+          className=" absolute lightSVG blob-splash "
+          style={{ height: "35vh" }}
+        /> */}
+        <SplashMedium className="h-screen absolute bottom-0 right-0 lightSVG" />
+        <SplashSmall
+          className="h-screen absolute small-splash lightSVG "
+          style={{ height: "35vh" }}
+        />
+      </div>{" "}
       {/* <div className="relative pt-16 pb-32 flex content-center items-center justify-center h-screen"> */}{" "}
       {/* <Image
         src="../images/code-background.png"
@@ -34,21 +56,12 @@ const LandingPage = () => {
         className="h-screen absolute bottom-0 lightSVG "
         style={{ height: "65vh", left: "5%" }}
       /> */}
-      <BlobSplash
-        className="h-screen absolute lightSVG blob-splash "
-        style={{ height: "35vh" }}
-      />
-      <SplashMedium className="h-screen absolute bottom-0 right-0 lightSVG" />
-      <SplashSmall
-        className="h-screen absolute small-splash lightSVG "
-        style={{ height: "35vh" }}
-      />
       {/* <Bird
         className="absolute opacity-75 bird"
         style={{ height: "22rem", fill: "white" }}
       />{" "} */}
       <div className=" absolute opacity-25 bottom-0 right-0 top-0 left-0" />
-      {/* <Gradient className="w-full h-full absolute bottom-0 right-0 top-0 left-0 " /> */}
+      <Gradient className="w-full h-full absolute bottom-0 right-0 top-0 left-0 " />
       {/* <div className="absolute top-0 w-full h-full bg-center bg-cover"> */}{" "}
       <div className=" w-full h-full absolute bottom-0 right-0 top-0 left-0">
         {" "}
@@ -58,7 +71,8 @@ const LandingPage = () => {
             Izabelle Wilding
           </h1>{" "}
           <p className="quicksand text-2xl md:text-4xl text-white">
-            Freelance Front-End Web Developer & Artist
+            Freelance Front-End Web Developer &{" "}
+            <em className="amatic">Artist</em>
           </p>
           <button
             class="absolute bottom-0 bg-transparent hover:bg-blue-500 text-blue-100 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded  quicksand"
